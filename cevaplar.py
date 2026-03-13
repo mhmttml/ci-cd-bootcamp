c3f1abed1b8b60729642fce3c23a97859d21c369
mhmttml <mtemel356@gmail.com>
2026-03-13 15:17:39 +0300

    Cevaplar yazıldı.

--- cevaplar.py
+++ cevaplar.py
@@ -11,33 +11,37 @@
 cevaplar = {
 
     # 🌀 Portal 1 — Hoşgeldin Portalı (Gülşah)
+<<<<<<< HEAD
     "vaka1": "",
+=======
+    "vaka1": "MERHABA",
+>>>>>>> e12cc84 (All answers has been written)
 
     # 🌀 Portal 2 — Sayı Matrisi (Serhat)
-    "vaka2": "",
+    "vaka2": "ALKU",
 
     # 🌀 Portal 3 — Makinelerin Dili (Mehmet)
-    "vaka3": "",
+    "vaka3": "CESARET",
 
     # 🌀 Portal 4 — Gizli Portal (Okan)
-    "vaka4": "",
+    "vaka4": "EVREN",
 
     # 🌀 Portal 5 — Koordinat Haritası (Nigina)
-    "vaka5": "",
+    "vaka5": "ROTA",
 
     # 🌀 Portal 6 — İrem'in Şifre Kasası (İrem)
-    "vaka6": "",
+    "vaka6": "ANAHTAR",
 
     # 🌀 Portal 7 — Şiirin Portalı (Kaya)
-    "vaka7": "",
+    "vaka7": "LABİRENT",
 
     # 🌀 Portal 8 — Mors Sinyalleri (Serhat)
-    "vaka8": "",
+    "vaka8": "ARAYIŞ",
 
     # 🌀 Portal 9 — Mantık Kapısı (Core Crew)
-    "vaka9": "",
+    "vaka9": "REHBER",
 
     # 🌀 Portal 10 — Son Portal (Core Crew)
-    "vaka10": "",
+    "vaka10": "MACERALAR",
 
 }
